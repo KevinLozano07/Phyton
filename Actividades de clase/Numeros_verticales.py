d72776e6b6c275cd6fc1957d4cfa7379c6413f4b
KevinLozano07 <kevin.lozano@tecnologicoliceocrisitano.edu.ec>
2024-08-01 21:18:47 -0500

    Actividad arreglos

---
+++ Actividades de clase/Numeros_verticales.py
@@ -0,0 +1,11 @@
+print("")
+N = int(input("Introduce el numero que iniciariara el ciclo desendente: "))
+print("")
+
+resultado = ""
+
+for i in range(N, 0, -1):
+    resultado += str(i) + " "
+
+print(resultado)
+print("")
---
+++ Arreglos/Unidimensionales-Vectores.py
@@ -0,0 +1,42 @@
+#Declara un alista que sea unidimensinal sobre frutas
+
+Frutas = ["Manzana", "Cereza", "Fresa", "Piña"]
+
+print("")
+print(Frutas)
+
+#.append agrega elemento al final de la lista
+Frutas.append("Sandia")
+
+print("")
+print(Frutas)
+
+#Acceder a un elemento
+print("")
+print(Frutas[2])
+
+#Cambiar un elemento
+Frutas[2] = "kiwi"
+
+print("")
+print(Frutas[2])
+
+#Ejercicio de ordenamineto
+
+Numeros = [5,2,4,1,3,7,8,9,10,6]
+
+print("")
+print(Numeros)
+
+Numeros.sort()
+print("")
+print(Numeros)
+
+DigitoTres = 5*[3]
+print("")
+print(DigitoTres)
+
+#Tamaño de la lista
+print("")
+print("Cantidad de frutas: " , len(Frutas))
+print("")
